0f344b4f3269ed14231fc021713f1068b6406300
Lubasha <parakhonya1@yandex.ru>
2024-07-09 21:40:05 +0300

    backgroundColor

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Color from './Color/Color'
+
+function App() {
+  
+
+  return (
+    <>
+      <Color />
+    </>
+  )
+}
+
+export default App
---
+++ src/Color/Color.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Color() {
+  
+    const [valueName, setValue] = useState('')
+    const [color, setColor] = useState('')
+
+    const inputValue = (e) => {
+        setValue(e.target.value)     
+    }
+
+    const styleBackground = (color) =>{
+        setColor(color)
+      }
+
+    useEffect(()=> {document.body.style.backgroundColor = color}, [color])
+
+    function hexToFgb (){
+      const input = valueName;
+      const hexRgb = '#[a-fA-F0-9]{6}'
+      if(input.match(hexRgb)){
+        const hexColor = input.replace('#', '')
+        const r = parseInt(hexColor.substring(0,2), 16)
+        const g = parseInt(hexColor.substring(0,2), 16)
+        const b = parseInt(hexColor.substring(0,2), 16)
+        return 'rgb(' + r + ',' + g + ',' + b + ')'
+      }else{
+        return 'ошибка!'
+      }
+
+    }
+
+  
+    
+  return (
+    <div >
+      <input name='colorHEX'onChange={inputValue} />
+      <input name='colorHEX' value={hexToFgb()} onChange={styleBackground(inputValue)}/>
+    </div>
+  )
+   
+}
